5c7698e5bc36560e725dae030b1fe036f95572aa
ki11errabbit <ki11errabbit@posteo.net>
2024-08-03 18:45:51 -0700

    reworking interpreter

--- definitions/src/class.rs
+++ definitions/src/class.rs
@@ -10,10 +10,11 @@ bitflags::bitflags! {
     #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
     pub struct ClassFlags: u8 {
         const Public = 0x01;
-        const Final = 0x02;
-        const Super = 0x20;
-        const Interface = 0x40;
-        const Abstract = 0x80;
+        const Private = 0x02;
+        const Protected = 0x04;
+        const Abstract = 0x08;
+        const Interface = 0x10;
+        const Static = 0x20;
     }
 }
 
@@ -37,6 +38,7 @@ pub enum PoolEntry {
     TypeInfo(TypeInfo),
     Redirect(PoolIndex),
     Reference(Reference),
+    Blank,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -71,8 +73,8 @@ pub enum TypeInfo {
     Array(Box<TypeInfo>),
     Object(PoolIndex),
     Method {
-        args: Vec<TypeInfo>,
-        ret: Box<TypeInfo>,
+        args: Vec<PoolIndex>,
+        ret: Box<PoolIndex>,
     },
 }
 
@@ -83,6 +85,12 @@ pub struct FieldInfo {
     pub type_info: PoolIndex,
     /// The location of the field's value in the object
     pub location: Option<PoolIndex>,
+}
+
+impl FieldInfo {
+    pub fn is_static(&self) -> bool {
+        self.flags.contains(FieldFlags::Static)
+    }
 }
 
 bitflags::bitflags! {
@@ -133,18 +141,18 @@ impl Default for InterfaceInfo {
     }
 }
 
-pub struct ClassHeaderBody {
-    this_info: PoolIndex,
-    parent_info: PoolIndex,
-    class_flags: ClassFlags,
-    constant_pool: Vec<PoolEntry>,
-    interfaces: Vec<InterfaceInfo>,
-    fields: Vec<FieldInfo>,
-    methods: Vec<MethodInfo>,
-    strings: Vec<PoolIndex>,
+pub struct ClassHeader {
+    pub this_info: PoolIndex,
+    pub parent_info: PoolIndex,
+    pub class_flags: ClassFlags,
+    pub constant_pool: Vec<PoolEntry>,
+    pub interfaces: Vec<InterfaceInfo>,
+    pub fields: Vec<FieldInfo>,
+    pub methods: Vec<MethodInfo>,
+    pub strings: Vec<PoolIndex>,
 }
 
-impl ClassHeaderBody {
+impl ClassHeader {
     pub fn new(constant_pool_size: usize, interfaces_count: usize, fields_count: usize, methods_count: usize, string_count: usize) -> Self {
         let mut constant_pool = Vec::with_capacity(constant_pool_size);
         constant_pool.resize_with(constant_pool_size, || PoolEntry::U8(0));
@@ -168,7 +176,7 @@ impl ClassHeaderBody {
         let mut strings = Vec::with_capacity(string_count);
         strings.resize_with(string_count, || 0);
         
-        ClassHeaderBody {
+        ClassHeader {
             this_info: 0,
             parent_info: 0,
             class_flags: ClassFlags::empty(),
@@ -177,213 +185,6 @@ impl ClassHeaderBody {
             fields,
             methods,
             strings,
-        }
-    }
-
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct ClassHeader(*mut ClassHeaderBody);
-
-impl ClassHeader {
-    pub fn new(constant_pool_size: usize, interfaces_count: usize, fields_count: usize, methods_count: usize, strings: usize) -> Self {
-        use std::alloc::{alloc, Layout};
-
-        let layout = Layout::new::<ClassHeaderBody>();
-        unsafe {
-            let ptr = alloc(layout) as *mut ClassHeaderBody;
-            ptr.write(ClassHeaderBody::new(constant_pool_size, interfaces_count, fields_count, methods_count, strings));
-            ClassHeader(ptr)
-        }
-    }
-
-    pub fn set_this_info(&mut self, this_info: PoolIndex) {
-        unsafe {
-            (*self.0).this_info = this_info;
-        }
-    }
-
-    pub fn get_this_info(&self) -> PoolIndex {
-        unsafe {
-            (*self.0).this_info
-        }
-    }
-
-    pub fn set_parent_info(&mut self, parent_info: PoolIndex) {
-        unsafe {
-            (*self.0).parent_info = parent_info;
-        }
-    }
-
-    pub fn get_parent_info(&self) -> PoolIndex {
-        unsafe {
-            (*self.0).parent_info
-        }
-    }
-
-    pub fn set_class_flags(&mut self, class_flags: ClassFlags) {
-        unsafe {
-            (*self.0).class_flags = class_flags;
-        }
-    }
-
-    pub fn get_class_flags(&self) -> ClassFlags {
-        unsafe {
-            (*self.0).class_flags
-        }
-    }
-
-    pub fn constant_pool_len(&self) -> usize {
-        unsafe {
-            (*self.0).constant_pool.len()
-        }
-    }
-
-    pub fn interfaces_count(&self) -> usize {
-        unsafe {
-            (*self.0).interfaces.len()
-        }
-    }
-
-    pub fn methods_count(&self) -> usize {
-        unsafe {
-            (*self.0).methods.len()
-        }
-    }
-
-    pub fn fields_count(&self) -> usize {
-        unsafe {
-            (*self.0).fields.len()
-        }
-    }
-
-    pub fn set_constant_pool_entry(&mut self, index: usize, entry: PoolEntry) {
-        unsafe {
-            (*self.0).constant_pool[index] = entry;
-        }
-    }
-
-    pub fn get_constant_pool_entry(&self, index: usize) -> &PoolEntry {
-        unsafe {
-            &(*self.0).constant_pool[index]
-        }
-    }
-
-    pub fn set_interface(&mut self, index: usize, interface: InterfaceInfo) {
-        unsafe {
-            (*self.0).interfaces[index] = interface;
-        }
-    }
-
-    pub fn get_interface(&self, index: usize) -> &InterfaceInfo {
-        unsafe {
-            &(*self.0).interfaces[index]
-        }
-    }
-
-    pub fn set_field(&mut self, index: usize, field: FieldInfo) {
-        unsafe {
-            (*self.0).fields[index] = field;
-        }
-    }
-
-    pub fn get_field(&self, index: usize) -> &FieldInfo {
-        unsafe {
-            &(*self.0).fields[index]
-        }
-    }
-
-    pub fn set_method(&mut self, index: usize, method: MethodInfo) {
-        unsafe {
-            (*self.0).methods[index] = method;
-        }
-    }
-
-    pub fn get_method(&self, index: usize) -> &MethodInfo {
-        unsafe {
-            &(*self.0).methods[index]
-        }
-    }
-
-    pub fn deallocate(&mut self) {
-        use std::alloc::{Layout, dealloc};
-        let layout = Layout::new::<ClassHeaderBody>();
-        let ptr = self.0 as *mut ClassHeaderBody;
-        unsafe {
-            core::ptr::drop_in_place(ptr);
-            dealloc(ptr as *mut u8, layout);
-        }
-    }
-
-    pub fn constants(&self) -> &[PoolEntry] {
-        unsafe {
-            &(*self.0).constant_pool
-        }
-    }
-
-    pub fn constants_mut(&mut self) -> &mut [PoolEntry] {
-        unsafe {
-            &mut (*self.0).constant_pool
-        }
-    }
-
-    pub fn interfaces(&self) -> &[InterfaceInfo] {
-        unsafe {
-            &(*self.0).interfaces
-        }
-    }
-
-    pub fn interfaces_mut(&mut self) -> &mut [InterfaceInfo] {
-        unsafe {
-            &mut (*self.0).interfaces
-        }
-    }
-
-    pub fn fields(&self) -> &[FieldInfo] {
-        unsafe {
-            &(*self.0).fields
-        }
-    }
-
-    pub fn fields_mut(&mut self) -> &mut [FieldInfo] {
-        unsafe {
-            &mut (*self.0).fields
-        }
-    }
-
-    pub fn methods(&self) -> &[MethodInfo] {
-        unsafe {
-            &(*self.0).methods
-        }
-    }
-
-    pub fn methods_mut(&mut self) -> &mut [MethodInfo] {
-        unsafe {
-            &mut (*self.0).methods
-        }
-    }
-
-    pub fn strings(&self) -> &[PoolIndex] {
-        unsafe {
-            &(*self.0).strings
-        }
-    }
-
-    pub fn strings_mut(&mut self) -> &mut [PoolIndex] {
-        unsafe {
-            &mut (*self.0).strings
-        }
-    }
-
-    pub fn get_string(&self, index: usize) -> PoolIndex {
-        unsafe {
-            (*self.0).strings[index]
-        }
-    }
-
-    pub fn set_string(&mut self, index: usize, string: PoolIndex) {
-        unsafe {
-            (*self.0).strings[index] = string;
         }
     }
 
@@ -401,67 +202,5 @@ mod tests {
 
     }
 
-    #[test]
-    fn test_class_header_set_this_info() {
-        let mut header = ClassHeader::new(10, 5, 3, 4, 0);
-        header.set_this_info(5);
-        assert_eq!(header.get_this_info(), 5);
-    }
-
-    #[test]
-    fn test_class_header_set_parent_info() {
-        let mut header = ClassHeader::new(10, 5, 3, 4, 0);
-        header.set_parent_info(5);
-        assert_eq!(header.get_parent_info(), 5);
-    }
-
-    #[test]
-    fn test_class_header_set_class_flags() {
-        let mut header = ClassHeader::new(10, 5, 3, 4, 0);
-        header.set_class_flags(ClassFlags::Public);
-        assert_eq!(header.get_class_flags(), ClassFlags::Public);
-    }
-
-    #[test]
-    fn test_class_header_set_constant_pool_entry() {
-        let mut header = ClassHeader::new(10, 5, 3, 4, 0);
-        header.set_constant_pool_entry(5, PoolEntry::String("Hello".to_owned()));
-        assert_eq!(*header.get_constant_pool_entry(5), PoolEntry::String("Hello".to_owned()));
-    }
-
-
-    #[test]
-    fn test_class_header_set_field() {
-        let mut header = ClassHeader::new(10, 5, 3, 4, 0);
-        header.set_field(2, FieldInfo {
-            name: 10,
-            flags: FieldFlags::Public,
-            type_info: 2,
-            location: None
-        });
-        assert_eq!(*header.get_field(2), FieldInfo {
-            name: 10,
-            flags: FieldFlags::Public,
-            type_info: 2,
-            location: None
-        });
-    }
-
-    #[test]
-    fn test_class_header_set_method() {
-        let mut header = ClassHeader::new(10, 5, 3, 4, 0);
-        header.set_method(3, MethodInfo {
-            name: 10,
-            flags: MethodFlags::Public,
-            type_info: 2,
-            location: 3
-        });
-        assert_eq!(*header.get_method(3), MethodInfo {
-            name: 10,
-            flags: MethodFlags::Public,
-            type_info: 2,
-            location: 3
-        });
-    }
     
 }
---
+++ definitions/src/object/class.rs
@@ -0,0 +1,91 @@
+use crate::class::{ClassFlags, ClassHeader, FieldInfo, MethodInfo, PoolEntry, PoolIndex};
+
+use super::Reference;
+
+
+
+
+pub struct ClassObjectBody {
+    this_info: PoolIndex,
+    parent_info: PoolIndex,
+    class_flags: ClassFlags,
+    constant_pool: Vec<PoolEntry>,
+    static_fields: Vec<FieldInfo>,
+    instance_fields: Vec<FieldInfo>,
+    methods: Vec<MethodInfo>,
+    strings: Vec<PoolIndex>,
+}
+
+impl ClassObjectBody {
+    pub fn new(this_info: PoolIndex, parent_info: PoolIndex, class_flags: ClassFlags, constant_pool: Vec<PoolEntry>, static_fields: Vec<FieldInfo>, instance_fields: Vec<FieldInfo>, methods: Vec<MethodInfo>, strings: Vec<PoolIndex>) -> ClassObjectBody {
+        ClassObjectBody {
+            this_info,
+            parent_info,
+            class_flags,
+            constant_pool,
+            static_fields,
+            instance_fields,
+            methods,
+            strings,
+        }
+    }
+
+    pub fn get_this_info(&self) -> PoolIndex {
+        self.this_info
+    }
+
+    pub fn get_parent_info(&self) -> PoolIndex {
+        self.parent_info
+    }
+
+    pub fn get_class_flags(&self) -> ClassFlags {
+        self.class_flags
+    }
+
+    pub fn get_constant_pool(&self) -> &Vec<PoolEntry> {
+        &self.constant_pool
+    }
+
+    pub fn get_static_fields(&self) -> &Vec<FieldInfo> {
+        &self.static_fields
+    }
+
+    pub fn get_instance_fields(&self) -> &Vec<FieldInfo> {
+        &self.instance_fields
+    }
+
+    pub fn get_methods(&self) -> &Vec<MethodInfo> {
+        &self.methods
+    }
+
+    pub fn get_strings(&self) -> &Vec<PoolIndex> {
+        &self.strings
+    }
+}
+
+
+impl From<ClassHeader> for ClassObjectBody {
+    fn from(header: ClassHeader) -> Self {
+        let mut static_fields = Vec::new();
+        let mut instance_fields = Vec::new();
+
+        for field in header.fields.into_iter() {
+            if field.is_static() {
+                static_fields.push(field.clone());
+            } else {
+                instance_fields.push(field.clone());
+            }
+        }
+        
+        ClassObjectBody {
+            this_info: header.this_info,
+            parent_info: header.parent_info,
+            class_flags: header.class_flags,
+            constant_pool: header.constant_pool,
+            static_fields,
+            instance_fields,
+            methods: header.methods,
+            strings: header.strings,
+        }
+    }
+}
